9fc650c214b18462a11febbfb693682ce009b0bc
6otho <124158269+6otho@users.noreply.github.com>
2026-06-07 23:29:10 +0800

    Update index.ts

--- src/crawler/index.ts
+++ src/crawler/index.ts
@@ -250,7 +250,7 @@ const GENRE_TRANSLATIONS: Record<GenreKey, Record<Locale, string>> = {
 };
 
 const GENRE_ITEMS: { id: string; key: GenreKey; imageName: string }[] = [
-  { id: "18", key: "drama", imageName: "drama.png" },
+  { id: "18", key: "drama", imageName: "Drama.png" },
   { id: "35", key: "comedy", imageName: "Comedy.png" },
   { id: "9648,53", key: "thriller", imageName: "Thriller.png" },
   { id: "28", key: "action", imageName: "Action.png" },
